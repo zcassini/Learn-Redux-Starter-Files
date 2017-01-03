ce02a4e14d1c0bb8c568f6858a39c7ed6ac834bd
Zac Cassini <zcassini@gmail.com>
2017-01-03 15:50:18 -0500

    thru tut 5

---
+++ learn-redux/client/componenets/Single.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Single = React.createClass({
+  render () {
+    return (
+      <div ClassName='single-photo'>
+        I'm the single photo.
+      </div>
+
+    )
+  }
+})
+
+export default Single
--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,25 @@
-// let's go!
+import React from 'react'
+
+import {render } from 'react-dom'
+
+// import css
+import css from './styles/style.styl'
+
+//import components
+import Main from './componenets/Main'
+import Single from './componenets/Single'
+import PhotoGrid from './componenets/PhotoGrid.js'
+
+// import react router deps
+import  { Router, Route, IndexRoute, browserHistory } from 'react-router'
+
+const router = (
+  <Router history = { browserHistory }>
+    <Route path='/' component={ Main }>
+      <IndexRoute component={ PhotoGrid } />
+      <Route path ='/view/:postId' component={ Single } />
+    </Route>
+  </Router>
+  )
+render(router, document.getElementById('root'))
+
